2bbead80863c896643a2ded2f511bc8cb181c8b9
BGMer7 <caijinyang98@gmail.com>
2021-12-16 21:35:29 +0800

    1610. 可见点的最大数目

---
+++ src/com/gatsby/_1610MaximumNumberOfVisiblePoints.java
@@ -0,0 +1,50 @@
+package com.gatsby;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author -- gatsby
+ * @date -- 2021/12/16
+ * @description -- leetcode 1610 可见点的最大数目 极坐标
+ */
+
+
+public class _1610MaximumNumberOfVisiblePoints {
+    public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
+        int sameCnt = 0;
+        List<Double> polarDegrees = new ArrayList<>();
+        int locationX = location.get(0);
+        int locationY = location.get(1);
+        for (int i = 0; i < points.size(); ++i) {
+            int x = points.get(i).get(0);
+            int y = points.get(i).get(1);
+            if (x == locationX && y == locationY) {
+                sameCnt++;
+                continue;
+            }
+            Double degree = Math.atan2(y - locationY, x - locationX);
+            polarDegrees.add(degree);
+        }
+        Collections.sort(polarDegrees);
+
+        int m = polarDegrees.size();
+        for (int i = 0; i < m; ++i) {
+            polarDegrees.add(polarDegrees.get(i) + 2 * Math.PI);
+        }
+
+        int maxCnt = 0;
+        int right = 0;
+        double toDegree = angle * Math.PI / 180;
+        for (int i = 0; i < m; ++i) {
+            Double curr = polarDegrees.get(i) + toDegree;
+            while (right < polarDegrees.size() && polarDegrees.get(right) <= curr) {
+                right++;
+            }
+            maxCnt = Math.max(maxCnt, right - i);
+        }
+        return maxCnt + sameCnt;
+    }
+
+}
