ff6bbc1ef654480e7ddf68e53e782b5058f17f1b
BGMer7 <caijinyang98@gmail.com>
2022-01-14 21:35:00 +0800

    373. 查找和最小的 K 对数字

---
+++ src/com/gatsby/lcp/LCP12CodingPlan.java
@@ -0,0 +1,15 @@
+package com.gatsby.lcp;
+
+/**
+ * @author -- gatsby
+ * @date -- 2022/1/12
+ * @description -- LCP 12 小张刷题计划
+ */
+
+
+public class LCP12CodingPlan {
+    public int minTime(int[] time, int m) {
+        // TODO
+        return 1;
+    }
+}
