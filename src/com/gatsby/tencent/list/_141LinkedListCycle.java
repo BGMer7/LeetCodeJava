d240a29361d9f0bbd52fa7e0afdf42318a64f5c3
BGMer7 <caijinyang98@gmail.com>
2022-03-14 22:28:51 +0800

    141. 环形链表

--- src/com/gatsby/tencent/list/_141LinkedListCycle.java
+++ src/com/gatsby/tencent/list/_141LinkedListCycle.java
@@ -27,7 +27,7 @@ public class _141LinkedListCycle {
         ListNode slow = head;
         ListNode fast = head;
 
-        while (fast != null || fast.next != null) {
+        while (fast != null && fast.next != null) {
             fast = fast.next.next;
             slow = slow.next;
             if (fast == slow) {
