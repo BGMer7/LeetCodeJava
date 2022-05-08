258c65903d111c319251c0be1d85679c24c7796c
Gatsby <caijinyang98@gmail.com>
2022-05-08 16:45:12 +0800

    69. x的平方根

--- src/com/gatsby/_69SqrtX.java
+++ src/com/gatsby/_69SqrtX.java
@@ -42,17 +42,17 @@ public class _69SqrtX {
         if (x == 0) return 0;
         if (x == 1) return 1;
 
-        int left = 1;
-        int right = x/2;
+        long left = 0;
+        long right = x;
         while (left < right) {
-            int mid = left + (right - left + 1) / 2;
+            long mid = left + (right - left + 1) / 2;
             if (mid > x / mid) {
                 right = mid - 1;
             } else {
                 left = mid;
             };
         }
-        return left;
+        return (int)left;
     }
 }
 
